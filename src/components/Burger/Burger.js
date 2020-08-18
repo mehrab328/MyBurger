b062adf5ed96de0f93756d37604ce9c43529331e
Mehrab Zaman <mehrab.zaman@outlook.com>
2020-08-18 13:18:45 +0600

    Navigating to the Contact Data Component

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -11,7 +11,7 @@ const burger = (props) => {
       return arr.concat(el);
   },[]);
 
-  if(transformedIngredients.length == 0) {
+  if(transformedIngredients.length === 0) {
     transformedIngredients = <p>Please start adding ingredients!</p>
   }
   return (
